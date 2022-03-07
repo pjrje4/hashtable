dc6dc1c85c5a33ad127464d6bbc3b10a09586528
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-03-07 09:05:30 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,81 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
-#include <iomanip> 
-
-/*
- *Justin Iness
- *11/17/2021
- *Student List
- * C++ Programming
- */
-
+#include <fstream>
 using namespace std;
-struct Student { //student struct
-	char firstName[20];
-	char lastName[20];
+
+struct student {
+	char* first;
+	char* last;
 	int id;
 	float gpa;
+	student(char* firstIn, char* lastIn) {
+		first = firstIn;
+		last = lastIn;
+	}
 };
-void addStudent(vector<Student*>*);
-void printStudent(vector<Student*>*);
-void deleteStudent(vector<Student*>*);
+struct item {
+	student* s = NULL;
+	item* next = NULL;
+	item(student* sIn) {
+		s = sIn;
+	}
+};
+struct hashT {
+	int tablelength = 0;
+	hashT(int length) {
+		item** items[length];
+		int tablelength = length;
+	}
+};
+
+int hashify(student* sIn, hashT* hashtable) {
+	return (sIn->id % hashtable->tablelength);
+}
 
 int main() {
-	vector<Student*>* students = new vector<Student*>();
 	while (true) { //loop
 		char input[20];
+		int gen;
+		cout << "How many students to generate (1-1000): ";
+		cin >> gen;
+
+		char firstNames [gen] [20];
+		char lastNames [gen] [20];
+		fstream firstfile;
+		firstfile.open("fnames.txt", ios::in);
+		if (!firstfile) {
+			cout << "Can't find fnames.txt." << endl;
+		}
+		else {
+			//char temp[20];
+			//char tempC;
+			char ch;
+			while (true) {
+				//tempC = 
+				//while 
+				firstfile >> ch;
+				if (firstfile.eof()) {
+					break;
+				}
+				cout << ch;
+			}
+
+		}
+
 		cout << "Enter a command (ADD, PRINT, DELETE, QUIT): ";
 		cin >> input;
 		if (strcmp(input, "ADD") == 0) { //add students
-			addStudent(students);
+			//addStudent(students);
 		}
 		else if (strcmp(input, "PRINT") == 0) { //print students
-			printStudent(students);
+			//printStudent(students);
 		}
 		else if (strcmp(input, "DELETE") == 0) { //delete students
-			deleteStudent(students);
+			//deleteStudent(students);
 		}
 		else if (strcmp(input, "QUIT") == 0) { //quit program
 			return 0;
         	}
 	}
+	return 0;
 }
