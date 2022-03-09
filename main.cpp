93fc143816ed22e3ffdcc62f4b49e0b6c6bc4f92
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-03-09 09:13:26 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 struct student {
@@ -32,6 +34,12 @@ int hashify(student* sIn, hashT* hashtable) {
 	return (sIn->id % hashtable->tablelength);
 }
 
+char* randomName(char names[][20], int length) {
+	srand(time(NULL));
+	int randN = rand() % length;
+	return names[randN];
+}
+
 int main() {
 	while (true) { //loop
 		char input[20];
@@ -41,29 +49,67 @@ int main() {
 
 		char firstNames [gen] [20];
 		char lastNames [gen] [20];
+
+		//firstnames
 		fstream firstfile;
 		firstfile.open("fnames.txt", ios::in);
 		if (!firstfile) {
 			cout << "Can't find fnames.txt." << endl;
 		}
 		else {
-			char temp[20];
 			char ch;
 			int reading = 0;
+			int firstIndex = 0;
 			while (reading <= gen) {
+				char temp[20] = {'\0'};
+				firstIndex = 0;
 				while (true) { 
 					firstfile >> ch;
-					if (firstfile.eof()) {
+					temp[firstIndex] = ch;
+					firstIndex++;
+					if (firstfile.eof()) { // end break
 						break;
 					}
-					cout << ch;
 					if (firstfile.peek() == '\n') {
 						break;
 					}
 				}
+				for (int i=0;i<=20;i++) {
+					firstNames[reading][i] = temp[i];
+				}
+				reading++;
 			}
-
 		}
+		//lastnames
+                fstream lastfile;
+                lastfile.open("lnames.txt", ios::in);
+                if (!lastfile) {
+                        cout << "Can't find lnames.txt." << endl;
+                }
+                else {
+                        char ch;
+                        int reading = 0;
+                        int lastIndex = 0;
+                        while (reading <= gen) {
+                                char temp[20] = {'\0'};
+                                lastIndex = 0;
+                                while (true) {
+                                        lastfile >> ch;
+                                        temp[lastIndex] = ch;
+                                        lastIndex++;
+                                        if (lastfile.eof()) { // end break
+                                                break;
+                                        }
+                                        if (lastfile.peek() == '\n') {
+                                                break;
+                                        }
+                                }
+                                for (int i=0;i<=20;i++) {
+                                        lastNames[reading][i] = temp[i];
+                                }
+                                reading++;
+                        }
+                }
 
 		cout << "Enter a command (ADD, PRINT, DELETE, QUIT): ";
 		cin >> input;
@@ -72,6 +118,7 @@ int main() {
 		}
 		else if (strcmp(input, "PRINT") == 0) { //print students
 			//printStudent(students);
+			cout << randomName(firstNames, gen) << " " << randomName(lastNames, gen) << endl;
 		}
 		else if (strcmp(input, "DELETE") == 0) { //delete students
 			//deleteStudent(students);
